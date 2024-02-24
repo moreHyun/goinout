6cf00c2c31169f557cf5b0658c1386269dfa52a0
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2024-02-24 19:56:20 +0900

    🍀20240224 Project Start

---
+++ src/test/java/com/goinout/GoinoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goinout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoinoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
